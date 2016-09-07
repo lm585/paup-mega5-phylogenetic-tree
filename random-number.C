eaa1497e56973c71d1ae42c4a360641dbe802da7
lm585 <lm585@users.noreply.github.com>
2016-09-06 20:12:50 -0400

    C++ generate a certain number of random numbers

---
+++ random-number.C
@@ -0,0 +1,147 @@
+/*
+ */
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstring>
+#include <cctype>
+#include <cstdlib>
+#include <ctime>
+#include <set>
+#include <vector>
+#include <sstream>
+#include <map>
+
+using namespace std;
+
+void readFile(const char * argv, ifstream & input);
+void writeFile(const char * argv, ofstream & output);
+void getFieldContent(vector<string> & fields, char del, string line);
+void getFieldContent2(vector<string> & fields, string del, string line);
+
+int main(int argc, char *argv[])
+{
+ if(argc != 3)
+ {
+  cerr << argv[0] << "  1967184      output_file" << endl;
+  cerr << "1967184 - number of random numbers needed" << endl;
+  return 1;
+ }
+
+ ifstream input;
+ ofstream output;
+ string line;
+ int count;
+ vector<string> lineFields;
+
+ count = atoi(argv[1]);
+ writeFile(argv[2], output);
+ cout << "RAND_MAX " << RAND_MAX << endl;
+ srand (time(NULL));
+ for(int i = 1; i <= count; i++)
+ {
+  output << rand() << endl;
+ }
+ output.close();
+ return 0;
+}
+
+void readFile(const char * argv, ifstream & input)
+{
+ input.open(argv, ios::in);
+ if(!input)
+ {
+  cerr << argv << " can not be opened for reading.\n";
+  exit(1);
+ }
+}
+
+void writeFile(const char * argv, ofstream & output)
+{
+ output.open(argv, ios::out);
+ if(!output)
+ {
+  cerr << argv << " can not be opened for writing.\n";
+  exit(1);
+ }
+}
+
+void getFieldContent(vector<string> & fields, char del, string line)
+{
+ vector<int> pos;
+ string str;
+ int len, size;
+
+ fields.clear();
+ for(int i = 0; i < line.length(); i++)
+ {
+  if(del == line[i])
+    pos.push_back(i);
+ }
+ if(pos.size() > 0)
+ {
+  len = pos[0];
+  str = line.substr(0, len);
+  if(len > 0)
+    fields.push_back(str);
+  for(int i = 0; i < pos.size() - 1; i++)
+  {
+   len = pos[i+1] - pos[i] - 1;
+   str = line.substr(pos[i] + 1, len);
+   fields.push_back(str);
+  }
+  size = pos.size();
+  if(pos[size-1] < line.length() - 1) //not at the end of line
+  {
+   str = line.substr(pos[size-1] + 1);
+   fields.push_back(str);
+  }
+ }
+ else
+ {
+  fields.push_back(line);
+ }
+}
+
+void getFieldContent2(vector<string> & fields, string del, string line)
+{
+ vector<int> pos;
+ string str;
+ int len, size;
+
+ fields.clear();
+ for(int i = 0; i < line.length(); i++)
+ {
+  if(del.find(line[i]) != string::npos)
+    pos.push_back(i);
+ }
+ if(pos.size() > 0)
+ {
+  len = pos[0];
+  str = line.substr(0, len);
+  if(len > 0)
+    fields.push_back(str);
+  for(int i = 0; i < pos.size() - 1; i++)
+  {
+   len = pos[i+1] - pos[i] - 1;
+   if(len > 0)
+   {
+    str = line.substr(pos[i] + 1, len);
+    fields.push_back(str);
+   }
+  }
+  size = pos.size();
+  if(pos[size-1] < line.length() - 1) //not at the end of line
+  {
+   str = line.substr(pos[size-1] + 1);
+   fields.push_back(str);
+  }
+ }
+ else
+ {
+  fields.push_back(line);
+ }
+}
+
+
